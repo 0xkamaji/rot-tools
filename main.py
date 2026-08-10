6c1f04e950e928c79e1053e96b30cfa7cdede949
0xkamaji <0xkamaji@proton.me>
2026-08-10 11:42:04 -0600

    added description in main.py for rotbot

---
+++ main.py
@@ -0,0 +1,7 @@
+############################################################
+# Author: Kamaji aka Deadhand aka gh0st aka scraps_dad
+# Creation Date: 08.10.26
+# Description:
+# This is my little rotten AI helper bot - rotbot. 
+# He manages signalrot and rotten signals in his domain
+############################################################
